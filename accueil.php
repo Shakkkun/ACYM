7f18194053b1b2a1996ef3d32dd38fbd9041eafc
Shakkun <monstershakuzu@gmail.com>
2020-04-21 17:04:30 +0200

    Responsive
    Responsive, quelque changement du css

--- accueil.php
+++ accueil.php
@@ -79,15 +79,14 @@
                         <label data-error="wrong" data-success="right" for="defaultForm-pass">Mot de passe</label>
                         <input type="password" id="defaultForm-pass" class="form-control validate" id="mdp_co" name="mdp_co" required>
                       </div>
-
+                    </div>
+                    <div>
+                      <a href="php/mot_de_passe_oublie.php">Mot de passe oublié ?</a>
                     </div>
                     <div class="modal-footer d-flex justify-content-center">
                       <input type="submit" id="connexion" name="connexion" value="Connexion" class="btn btn-default">
-
                     </div>
-
                     </form>
-
                     </div>
                     </div>
                     </div>
@@ -98,7 +97,7 @@
               </li> 
           </ul>
     </header>
-    <img class=con src="../ACYM/img/Wow.jpg">
+    <img class=con src="../ACYM/img/Wow.jpg" id="Merci_Axel">
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
---
+++ php/mot_de_passe_oublie.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/mdp.css" rel="stylesheet">    
+    <title>Animal Crossing : Your Museum</title>
+
+
+<h1> Mot de passe oublié ?</h1>
+<div class="centrer">
+<form>
+<p>Veuillez rentrez votre mail</p>
+<input type="" id="" class="form-control validate" id="" name="" required>
+<button>Valider</button>
+</form>
+</div>
